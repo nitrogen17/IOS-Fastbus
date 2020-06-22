2426906c659f0628264a9e7b847ea8599d65070e
Meta <rfmetagm@gmail.com>
2020-06-22 20:52:57 +0800

    Initial Commit

---
+++ FastBus/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FastBus
+//
+//  Created by Jonorsky Navarrete on 6/22/20.
+//  Copyright © 2020 Nitrogen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
