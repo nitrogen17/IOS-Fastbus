740519aff2adb272a2019e8122d98682cc73a4e8
Nitrogen <jonorskydev@gmail.com>
2020-06-26 22:26:49 +0800

    Added and Enhance UI Login Controller

--- FastBus/ViewController.swift
+++ FastBus/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        view.backgroundColor = .black
+//        view.backgroundColor = .black
     }
 
     @IBAction func clickButton(_ sender: UIButton) {
