c103b6bb5f69db1c7869a3f515fd1ca3ac6a1a2d
Nitrogen <jonorskydev@gmail.com>
2020-07-07 20:29:59 +0800

    Added .gitignore

--- FastBus/Controller/MenuViewController.swift
+++ FastBus/Controller/MenuViewController.swift
@@ -13,32 +13,32 @@ class MenuViewController: UIViewController {
     /// Reference from IB
     @IBOutlet weak var topViewRef: UIView!
     
-
+    
     private var shadowLayer: CAShapeLayer!
     private var cornerRadius: CGFloat = 25.0
     private var fillColor: UIColor = .blue // the color applied to the shadowLayer, rather than the view's backgroundColor
-     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         
-         /// Add Corner Radius in UIView
-         topViewRef.clipsToBounds = true
-         topViewRef.layer.cornerRadius = 8
+        /// Add Corner Radius in UIView
+        //topViewRef.clipsToBounds = true
+        //topViewRef.layer.cornerRadius = 8
+        
+            
+        topViewRef.layer.shadowColor = UIColor.black.cgColor
+        topViewRef.layer.shadowOpacity = 0.1
+        topViewRef.layer.shadowOffset = .zero
+        topViewRef.layer.shadowRadius = 10
         
         /*
-         topViewShadow.layer.shadowColor = UIColor.black.cgColor
-         topViewShadow.layer.shadowOpacity = 0.9
-         topViewShadow.layer.shadowOffset = .zero
-         topViewShadow.layer.shadowRadius = 10
- */
-     /*
-        topViewRef.layer.shadowColor = UIColor.black.cgColor
-        topViewRef.layer.shadowOffset = CGSize(width: 3, height: 3)
-        topViewRef.layer.shadowOpacity = 0.7
-        topViewRef.layer.shadowRadius = 4.0
- 
- */
+         topViewRef.layer.shadowColor = UIColor.black.cgColor
+         topViewRef.layer.shadowOffset = CGSize(width: 3, height: 3)
+         topViewRef.layer.shadowOpacity = 0.7
+         topViewRef.layer.shadowRadius = 4.0
+         
+         */
         
     }
 }
