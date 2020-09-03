f558b659b16a4a496a575c3057ba779499925893
Nitrogen <jonorskydev@gmail.com>
2020-09-03 22:34:39 +0800

    Added Height for Reusable Table Cell

--- FastBus/Controller/BusListingVC.swift
+++ FastBus/Controller/BusListingVC.swift
@@ -73,4 +73,8 @@ extension BusListingVC: UITableViewDataSource, UITableViewDelegate {
         return cell!
     }
 
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 98
+    }
+
 }
