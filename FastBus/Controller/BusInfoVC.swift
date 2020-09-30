b402bbd6f84ff7985bff1441562566beccc1a786
Nitrogen <jonorskydev@gmail.com>
2020-09-30 23:57:17 +0800

    Added Controller in Bus Info

---
+++ FastBus/Controller/BusInfoVC.swift
@@ -0,0 +1,17 @@
+//
+//  BusInfoVC.swift
+//  FastBus
+//
+//  Created by Jonorsky Navarrete on 9/30/20.
+//  Copyright © 2020 Nitrogen. All rights reserved.
+//
+
+import UIKit
+
+class BusInfoVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
