31a9f7f640273f8ca191585d330be8e751ff4886
Nitrogen <jonorskydev@gmail.com>
2020-10-21 22:11:09 +0800

    Added Table View Cell with .xib

---
+++ FastBus/Controller/BusListCell.swift
@@ -0,0 +1,28 @@
+//
+//  BusListCell.swift
+//  FastBus
+//
+//  Created by Jonorsky Navarrete on 10/21/20.
+//  Copyright © 2020 Nitrogen. All rights reserved.
+//
+
+import UIKit
+
+class BusListCell: UITableViewCell {
+
+    @IBOutlet weak var labelCell: UILabel!
+    @IBOutlet weak var resultCell: UILabel!
+    @IBOutlet weak var priceCell: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
