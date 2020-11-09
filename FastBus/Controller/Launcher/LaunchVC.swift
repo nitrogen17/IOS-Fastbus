ddbd9f4c24624698ca3f124d7805bbd0641f3810
Nitrogen <jonorskydev@gmail.com>
2020-11-09 18:45:45 +0800

    Clean some comments and adjust DispatchQueue Brackets

--- FastBus/Controller/Launcher/LaunchVC.swift
+++ FastBus/Controller/Launcher/LaunchVC.swift
@@ -39,19 +39,8 @@ class LaunchVC: UIViewController {
 
             self.spinner.stopAnimating()
 
-//            self.appTitle.text = ""
-//            var charIndex = 0.0
-//            let titleText = "FASTBUS APP"
-//            for letter in titleText {
-//                Timer.scheduledTimer(withTimeInterval: 0.2 * charIndex, repeats: false) { (_) in
-//                    self.appTitle.text?.append(letter)
-//                }
-//                charIndex += 1
-//            }
-
-            DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
-
-            }
+            /// Delay for 3 seconds
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) { }
 
             DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
                 UIView.animate(withDuration: 0.7, animations: {
