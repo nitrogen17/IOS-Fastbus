f8e7f5b523014a1e0c9984368bea27ab7d720e1c
Nitrogen <jonorskydev@gmail.com>
2020-12-03 09:29:08 +0800

    Added UIViewController for UIView Bus Confirmation

---
+++ FastBus/Controller/Bus+Confirmation/BusConfirmationVC.swift
@@ -0,0 +1,20 @@
+//
+//  BusConfirmationVC.swift
+//  FastBus
+//
+//  Created by Jonorsky Navarrete on 12/3/20.
+//  Copyright © 2020 Nitrogen. All rights reserved.
+//
+
+import UIKit
+
+class BusConfirmationVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func performUnwindSegue(_ sender: Any) {
+        print("EXECUTE!")
+    }
+}
